f1b043e93320576a2e882a7745f581223accc8af
Tom Tromey <tromey@redhat.com>
2008-10-20 10:59:23 -0600

    * python/python-value.c (value_to_value_object): Initialize 	owned_by_gdb field. 	(valpy_new): Likewise.

--- gdb/python/python-value.c
+++ gdb/python/python-value.c
@@ -106,6 +106,7 @@ valpy_new (PyTypeObject *subtype, PyObject *args, PyObject *keywords)
     }
 
   value_obj->value = value;
+  value_obj->owned_by_gdb = 0;
   release_value (value);
   value_prepend_to_list (&values_in_python, value);
 
@@ -671,6 +672,7 @@ value_to_value_object (struct value *val)
   if (val_obj != NULL)
     {
       val_obj->value = val;
+      val_obj->owned_by_gdb = 0;
       release_value (val);
       value_prepend_to_list (&values_in_python, val);
     }
