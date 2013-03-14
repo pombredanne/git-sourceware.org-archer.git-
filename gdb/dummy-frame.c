e2b7cdbfdbfe29245cfda995fd24c7f9afadb054
Tom Tromey <tromey@redhat.com>
2013-03-14 11:33:47 -0600

    constify dummy-frame.c

--- gdb/dummy-frame.c
+++ gdb/dummy-frame.c
@@ -19,13 +19,13 @@
 
 
 #include "defs.h"
+#include "const-command.h"
 #include "dummy-frame.h"
 #include "regcache.h"
 #include "frame.h"
 #include "inferior.h"
 #include "gdb_assert.h"
 #include "frame-unwind.h"
-#include "command.h"
 #include "gdbcmd.h"
 #include "gdb_string.h"
 #include "observer.h"
@@ -304,7 +304,7 @@ fprint_dummy_frames (struct ui_file *file)
 }
 
 static void
-maintenance_print_dummy_frames (char *args, int from_tty)
+maintenance_print_dummy_frames (const char *args, int from_tty)
 {
   if (args == NULL)
     fprint_dummy_frames (gdb_stdout);
