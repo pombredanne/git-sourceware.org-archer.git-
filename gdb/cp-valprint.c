f3c24f5fe5e13417063d166583b29acbea957a9c
Tom Tromey <tromey@redhat.com>
2013-03-14 11:09:26 -0600

    constify cp-valprint.c

--- gdb/cp-valprint.c
+++ gdb/cp-valprint.c
@@ -18,12 +18,12 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "defs.h"
+#include "const-command.h"
 #include "gdb_obstack.h"
 #include "symtab.h"
 #include "gdbtypes.h"
 #include "expression.h"
 #include "value.h"
-#include "command.h"
 #include "gdbcmd.h"
 #include "demangle.h"
 #include "annotate.h"
