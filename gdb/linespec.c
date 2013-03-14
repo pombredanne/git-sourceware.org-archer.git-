951b8f7962fffec4dfb74d893189a5b565680db0
Tom Tromey <tromey@redhat.com>
2013-03-14 10:54:26 -0600

    constify linespec.c

--- gdb/linespec.c
+++ gdb/linespec.c
@@ -17,6 +17,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#define CLI_CONST const
+
 #include "defs.h"
 #include "symtab.h"
 #include "frame.h"
