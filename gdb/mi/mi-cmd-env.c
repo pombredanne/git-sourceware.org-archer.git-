45e302d46136ff64895a385ccf973d55212b6c3b
Tom Tromey <tromey@redhat.com>
2013-03-14 11:30:51 -0600

    constify mi-cmd-env.c

--- gdb/mi/mi-cmd-env.c
+++ gdb/mi/mi-cmd-env.c
@@ -19,6 +19,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "defs.h"
+#include "const-command.h"
 #include "inferior.h"
 #include "value.h"
 #include "mi-out.h"
@@ -27,7 +28,6 @@
 #include "symtab.h"
 #include "target.h"
 #include "environ.h"
-#include "command.h"
 #include "ui-out.h"
 #include "top.h"
 
