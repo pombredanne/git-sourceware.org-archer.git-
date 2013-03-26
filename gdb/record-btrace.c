668a1f3cd60544281462becb6c7695a8bfaa6304
Markus Metzger <markus.t.metzger@intel.com>
2013-03-26 11:32:20 +0100

    record-btrace: fix insn range in function call history
    With the "/i" modifier, we print the instruction number range in the
    "record function-call-history" command as [begin, end).
    
    It would be more intuitive if we printed the range as [begin, end].
    
    Signed-off-by: Markus Metzger  <markus.t.metzger@intel.com>
    
    	* record-btrace.c (btrace_call_history_insn_range): Print
    	insn range as [begin, end].
    
    Change-Id: If2d5768eb1604bbc1596a9534d9646630e9465eb

--- gdb/record-btrace.c
+++ gdb/record-btrace.c
@@ -418,10 +418,14 @@ static void
 btrace_call_history_insn_range (struct ui_out *uiout,
 				const struct btrace_function *bfun)
 {
-  unsigned int begin, end;
+  unsigned int begin, end, size;
+
+  size = VEC_length (btrace_insn_s, bfun->insn);
+  if (size == 0)
+    return;
 
   begin = bfun->insn_offset;
-  end = begin + VEC_length (btrace_insn_s, bfun->insn);
+  end = begin + size - 1;
 
   ui_out_field_uint (uiout, "insn begin", begin);
   ui_out_text (uiout, "-");
