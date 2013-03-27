f3f4f26ac1cbd9405807d5e9d4fb26f65a27d520
Aleksandar Ristovski <ARistovski@qnx.com>
2013-03-27 09:40:24 -0400

    Create empty common/linux-maps.[ch] and common/common-target.[ch]
    	* Makefile.in (HFILES_NO_SRCDIR); Add common/linux-maps.h,
    	common/common-target.h.
    	(COMMON_OBS): Add common-target.o.
    	(linux-maps.o, common-target.o): New.
    	* common/common-target.c: New file.
    	* common/common-target.h: New file.
    	* common/linux-maps.c: New file.
    	* common/linux-maps.h: New file.
    	* config/i386/linux.mh (NATDEPFILES): Add linux-maps.o.
    	* config/i386/linux64.mh (NATDEPFILES): Ditto.
    	* gdbserver/Makefile.in (OBS): Add common-taret.o.
    	(linux-maps.o, common-target.o): New.
    	* gdbserver/configure.srv (srv_tgtobj): Add linux-maps.o.

---
+++ gdb/common/common-target.h
@@ -0,0 +1,24 @@
+/* Utility target functions for GDB, and GDBserver.
+
+   Copyright (C) 2013 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef COMMON_COMMON_TARGET_H
+#define COMMON_COMMON_TARGET_H
+
+#endif /* COMMON_COMMON_TARGET_H */
+
