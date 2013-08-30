29fd0a2260d347d9473a0d321bcae502d949b697
Gary Benson <gbenson@redhat.com>
2013-08-30 13:23:54 +0100

    Reproducer

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <demangle.h>
+
+const char *symbol = "_ZSt7forwardIRN1x14refobjiteratorINS0_3refINS0_" \
+  "4mime30multipart_section_processorObjIZ15get_body_parserIZZN14mime" \
+  "_processor21make_section_iteratorERKNS2_INS3_10sectionObjENS0_10pt" \
+  "rrefBaseEEEbENKUlvE_clEvEUlSB_bE_ZZNS6_21make_section_iteratorESB_" \
+  "bENKSC_clEvEUlSB_E0_ENS1_INS2_INS0_20outputrefiteratorObjIiEES8_EE" \
+  "EERKSsSB_OT_OT0_EUlmE_NS3_32make_multipart_default_discarderISP_EE" \
+  "EES8_EEEEEOT_RNSt16remove_referenceISW_E4typeE";
+
+int
+main (int argc, char *argv[])
+{
+  puts (cplus_demangle (symbol, DMGL_AUTO | DMGL_ANSI | DMGL_PARAMS));
+}
