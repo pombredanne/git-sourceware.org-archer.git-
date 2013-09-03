5d900bfa80e0254a4bb304f149bd4b364971322b
Gary Benson <gbenson@redhat.com>
2013-09-03 09:31:36 +0100

    Let's take this apart

---
+++ test.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    demangle("_ZSt7forwardIRN1x14refobjiteratorINS0_3refINS0_4mime30mul"
+             "tipart_section_processorObjIZ15get_body_parserIZZN14mime_"
+             "processor21make_section_iteratorERKNS2_INS3_10sectionObjE"
+             "NS0_10ptrrefBaseEEEbENKUlvE_clEvEUlSB_bE_ZZNS6_21make_sec"
+             "tion_iteratorESB_bENKSC_clEvEUlSB_E0_ENS1_INS2_INS0_20out"
+             "putrefiteratorObjIiEES8_EEEERKSsSB_OT_OT0_EUlmE_NS3_32mak"
+             "e_multipart_default_discarderISP_EEEES8_EEEEEOT_RNSt16rem"
+             "ove_referenceISW_E4typeE")
